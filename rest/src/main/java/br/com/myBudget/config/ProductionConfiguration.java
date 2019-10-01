d6a6168704554da79bd504c4676aa0340fe61abc
marcio sansone <msansone@gmail.com>
2019-10-01 19:41:12 +0100

    first commit

---
+++ rest/src/main/java/br/com/myBudget/config/ProductionConfiguration.java
@@ -0,0 +1,10 @@
+package br.com.myBudget.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("production")
+public class ProductionConfiguration {
+
+}
